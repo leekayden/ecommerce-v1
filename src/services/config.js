5ceab4f7f8b08a3e22c96e4ade0fdc12965a803a
leekayden <kayden@programmer.net>
2023-06-23 20:54:34 +0800

    update

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,7 @@
 const firebaseConfig = {
   apiKey: "AIzaSyCUmfPy7S8upx8Hp1C5HSyFCogHExjAGiQ",
   authDomain: "ecommerce-v1-ab9e2.firebaseapp.com",
+  databaseURL: "https://ecommerce-v1-ab9e2-default-rtdb.firebaseio.com",
   projectId: "ecommerce-v1-ab9e2",
   storageBucket: "ecommerce-v1-ab9e2.appspot.com",
   messagingSenderId: "811810661772",
