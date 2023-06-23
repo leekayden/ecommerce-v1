efd2aade994b78cbc9475f27f2e036a59ce6c1a0
leekayden <kayden@programmer.net>
2023-06-23 18:12:35 +0800

    Initial commit

---
+++ src/services/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCUmfPy7S8upx8Hp1C5HSyFCogHExjAGiQ",
+  authDomain: "ecommerce-v1-ab9e2.firebaseapp.com",
+  projectId: "ecommerce-v1-ab9e2",
+  storageBucket: "ecommerce-v1-ab9e2.appspot.com",
+  messagingSenderId: "811810661772",
+  appId: "1:811810661772:web:6f81b893aebe992af822a9",
+  measurementId: "G-N0XCG0JVRK",
+};
+
+export default firebaseConfig;
